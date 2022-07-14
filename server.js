6208c5943fe0f5c8aea79d7be68e10e663745372
RominaVoscio <89428394+RominaVoscio@users.noreply.github.com>
2022-07-14 20:12:52 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express= require('express')
+const Contenedor= require('./contenedor/contenedor')
+const app= express()
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+const db= new Contenedor('db.json')
+app.get('/', (req,res)=>res.sendFile(__dirname + '/views/form.html'))
+app.post('/products', (req,res)=>{
+    db.save(req.body)
+    .then(()=> res.redirect('/'))
+    .catch(e=>{
+        console.log(e)
+        res.send('Error to save')
+    })
+})
+app.get('/products', (req,res)=>{ //carga del archivo
+    db.getData()
+    .then(data=> res.render('products', {data}))
+    .catch(e=>{
+        console.log(e)
+        res.send('Error to load data')
+    })
+})
+app.listen(8080)
